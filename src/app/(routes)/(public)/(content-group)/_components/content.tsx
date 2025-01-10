c2ccb6ae8fc957365823df6cd6cfd3b0803acb39
Abdallah-sala7 <Abdallah-Sala7@users.noreply.github.com>
2025-01-10 17:14:38 +0200

    Text Markdown

--- src/app/(routes)/(public)/(content-group)/_components/content.tsx
+++ src/app/(routes)/(public)/(content-group)/_components/content.tsx
@@ -1,7 +1,15 @@
+import ReactMarkdown from "react-markdown";
+
 type ContentProps = {
   content: string;
 };
 
 export const Content = ({ content }: ContentProps) => {
-  return <section className="bg-gray-100 rounded-2.5xl border border-gray-300 container py-6">{content}</section>;
+  return (
+    <section className="bg-gray-100 rounded-2.5xl border border-gray-300 container py-6">
+      <div>
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
+    </section>
+  );
 };
