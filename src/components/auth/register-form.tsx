fba8276fd85068ec550757658614b29a6ed27c96
Abdallah-sala7 <Abdallah-Sala7@users.noreply.github.com>
2025-01-14 00:07:12 +0200

    Fix terms-conditions Link

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -202,7 +202,7 @@ export const RegisterForm = () => {
                       </FormControl>
                       <FormLabel className="text-garyClr -translate-y-[0.2rem]">
                         <span>{t("privacy_policy.aggree")}</span>{" "}
-                        <Link href="/privacy-policy" className="text-primaryClr font-medium hover:underline">
+                        <Link href="/terms-conditions" className="text-primaryClr font-medium hover:underline">
                           <span>{t("privacy_policy.terms")}</span>
                         </Link>
                       </FormLabel>
